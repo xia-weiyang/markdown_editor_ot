420d5715c0327dac836eec1a81365dff6a3d0f8e
guowang <gw@jiushig.com>
2018-09-02 20:36:08 +0800

    Add actions

---
+++ lib/action.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+///  Action image button of markdown editor.
+class ActionImage extends StatefulWidget {
+  ActionImage({
+    Key key,
+    this.type,
+    this.tap,
+  }) : super(key: key);
+
+  final ActionType type;
+  final TapFinishCallback tap;
+
+  @override
+  ActionImageState createState() => ActionImageState();
+}
+
+class ActionImageState extends State<ActionImage> {
+  String _getImagePath() {
+    return _defaultImageAttributes
+        .firstWhere((img) => img.type == widget.type)
+        ?.path;
+  }
+
+  void _disposeAction() {
+    var firstWhere =
+        _defaultImageAttributes.firstWhere((img) => img.type == widget.type);
+    if (widget.tap != null && firstWhere != null) {
+      widget.tap(firstWhere.text, firstWhere.positionReverse);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Image(
+        width: 30.0,
+        height: 30.0,
+        image: AssetImage(_getImagePath(), package: 'markdown_editor'),
+      ),
+      tooltip: _defaultImageAttributes
+          .firstWhere((img) => img.type == widget.type)
+          ?.tip,
+      onPressed: _disposeAction,
+    );
+  }
+}
+
+const _defaultImageAttributes = <ImageAttributes>[
+  ImageAttributes(
+    type: ActionType.image,
+    path: 'images/edit_img.png',
+    text: '![]()',
+    tip: '图片',
+    positionReverse: 3,
+  ),
+  ImageAttributes(
+    type: ActionType.link,
+    path: 'images/link_img.png',
+    text: '[]()',
+    tip: '链接',
+    positionReverse: 3,
+  ),
+  ImageAttributes(
+    type: ActionType.fontBold,
+    path: 'images/format_bold_img.png',
+    text: '****',
+    tip: '加粗',
+    positionReverse: 2,
+  ),
+  ImageAttributes(
+    type: ActionType.fontItalic,
+    path: 'images/format_italic_img.png',
+    text: '**',
+    tip: '斜体',
+    positionReverse: 1,
+  ),
+//  ImageAttributes(
+//    type: ActionType.fontDeleteLine,
+//    path: 'images/strikethrough_img.png',
+//    text: '~~~~',
+//    tip: '删除线',
+//    positionReverse: 2,
+//  ),
+  ImageAttributes(
+    type: ActionType.textQuote,
+    path: 'images/format_quote_img.png',
+    text: '\n>',
+    tip: '文字引用',
+    positionReverse: 0,
+  ),
+  ImageAttributes(
+    type: ActionType.list,
+    path: 'images/list_img.png',
+    text: '\n- ',
+    tip: '无序列表',
+    positionReverse: 0,
+  ),
+  ImageAttributes(
+    type: ActionType.h4,
+    path: 'images/format_header_4_img.png',
+    text: '\n#### ',
+    tip: '四级标题',
+    positionReverse: 0,
+  ),
+  ImageAttributes(
+    type: ActionType.h5,
+    path: 'images/format_header_5_img.png',
+    text: '\n##### ',
+    tip: '五级标题',
+    positionReverse: 0,
+  ),
+  ImageAttributes(
+    type: ActionType.h1,
+    path: 'images/format_header_1_img.png',
+    text: '\n# ',
+    tip: '一级标题',
+    positionReverse: 0,
+  ),
+  ImageAttributes(
+    type: ActionType.h2,
+    path: 'images/format_header_2_img.png',
+    text: '\n## ',
+    tip: '二级标题',
+    positionReverse: 0,
+  ),
+  ImageAttributes(
+    type: ActionType.h3,
+    path: 'images/format_header_3_img.png',
+    text: '\n### ',
+    tip: '三级标题',
+    positionReverse: 0,
+  ),
+];
+
+enum ActionType {
+  image,
+  link,
+  fontBold,
+  fontItalic,
+  fontDeleteLine,
+  textQuote,
+  list,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+}
+
+class ImageAttributes {
+  const ImageAttributes({
+    this.tip = '',
+    this.text,
+    this.positionReverse,
+    @required this.type,
+    @required this.path,
+  })  : assert(path != null),
+        assert(type != null);
+
+  final ActionType type;
+  final String path;
+  final String tip;
+  final String text;
+  final int positionReverse;
+}
+
+/// Call this method after clicking the [ActionImage] and completing a series of actions.
+/// [text] Adding text.
+/// [position] Cursor position that reverse order.
+typedef TapFinishCallback(String text, int positionReverse);
--- lib/editor.dart
+++ lib/editor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:markdown_editor/action.dart';
 
 class MdEditor extends StatefulWidget {
   MdEditor({
@@ -43,52 +44,133 @@ class MdEditorState extends State<MdEditor> {
     _textEditingController.text = widget.initText ?? '';
   }
 
+  void _disposeText(String text, int index) {
+    _textEditingController.text += text;
+    _textEditingController.selection = TextSelection.collapsed(
+        offset: _textEditingController.text.length - index);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: widget.padding,
-        child: Column(
-          children: <Widget>[
-            TextField(
-              maxLines: 1,
-              controller: _titleEditingController,
-              style: widget.titleStyle ??
-                  TextStyle(
-                    fontSize: 20.0,
-                    color: const Color(0xFF333333),
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 50.0),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: widget.padding,
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    maxLines: 1,
+                    controller: _titleEditingController,
+                    style: widget.titleStyle ??
+                        TextStyle(
+                          fontSize: 20.0,
+                          color: const Color(0xFF333333),
+                        ),
+                    decoration: InputDecoration(
+                      hintText: widget.hintTitle ?? '标题',
+                      border: InputBorder.none,
+                    ),
                   ),
-              decoration: InputDecoration(
-                hintText: widget.hintTitle ?? '标题',
-                border: InputBorder.none,
+                  Container(
+                    height: 1.0,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFDDDDDD),
+                    ),
+                  ),
+                  TextField(
+                    maxLines: _maxLines,
+                    controller: _textEditingController,
+                    onChanged: (text) {
+                      if (_maxLines != null &&
+                          text != null &&
+                          text.length > _maxLines) {
+                        setState(() {
+                          _maxLines = null;
+                        });
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: widget.hintText ?? '请输入内容',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ],
               ),
             ),
-            Container(
-              height: 1.0,
-              decoration: BoxDecoration(
-                color: const Color(0xFFDDDDDD),
-              ),
-            ),
-            TextField(
-              maxLines: _maxLines,
-              controller: _textEditingController,
-              onChanged: (text) {
-                if (_maxLines != null &&
-                    text != null &&
-                    text.length > _maxLines) {
-                  setState(() {
-                    _maxLines = null;
-                  });
-                }
-              },
-              decoration: InputDecoration(
-                hintText: widget.hintText ?? '请输入内容',
-                border: InputBorder.none,
-              ),
-            ),
-          ],
+          ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                height: 50.0,
+                child: Ink(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF0F0F0),
+                    boxShadow: [
+                      BoxShadow(color: const Color(0xAAF0F0F0)),
+                    ],
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      ActionImage(
+                        type: ActionType.image,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.link,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.fontBold,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.fontItalic,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.textQuote,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.link,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.h4,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.h5,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.h1,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.h2,
+                        tap: _disposeText,
+                      ),
+                      ActionImage(
+                        type: ActionType.h3,
+                        tap: _disposeText,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
